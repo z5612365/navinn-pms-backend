51e53d2702aced3d8a244913846d5309db3b4f65
張永麒19660 <ESB19660@esunbank.com.tw>
2021-12-17 17:48:10 +0800

    add search api

---
+++ navinn-ap-controller/navinn-ap-customer-controller/src/main/java/persistent/model/bean/PaymentPo.java
@@ -0,0 +1,53 @@
+package persistent.model.bean;
+
+import java.math.BigDecimal;
+
+public class PaymentPo {
+
+    private String roomSeq;
+    private String paymentKey;
+    private String roomName;
+    private BigDecimal totalAmount;
+    private String status;
+
+    public String getRoomSeq() {
+        return roomSeq;
+    }
+
+    public void setRoomSeq(String roomSeq) {
+        this.roomSeq = roomSeq;
+    }
+
+    public String getPaymentKey() {
+        return paymentKey;
+    }
+
+    public void setPaymentKey(String paymentKey) {
+        this.paymentKey = paymentKey;
+    }
+
+    public String getRoomName() {
+        return roomName;
+    }
+
+    public void setRoomName(String roomName) {
+        this.roomName = roomName;
+    }
+
+    public BigDecimal getTotalAmount() {
+        return totalAmount;
+    }
+
+    public void setTotalAmount(BigDecimal totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+}
